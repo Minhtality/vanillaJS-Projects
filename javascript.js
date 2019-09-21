fc059dc611c8de3ecda16d0f4ca1a6c07cf1001d
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-20 18:09:36 -0700

    local storage

--- javascript.js
+++ javascript.js
@@ -7,6 +7,35 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', () => {
+        //get tasks from local storage
+        let tasks;
+        if (localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+
+        tasks.forEach(task => {
+            //create the li
+            const li = document.createElement('li');
+            // insert text in li
+            li.appendChild(document.createTextNode(task));
+            //insert class into li
+            li.className = 'collection-item'
+            //create new link
+            const link = document.createElement('a');
+            //link class
+            link.className = 'delete-item secondary-content'
+            //insert into link for inner html for icon
+            link.innerHTML = '<i class="fa fa-remove"></i>'
+            //shove link to li
+            li.appendChild(link)
+            //shove li into ul 
+            taskList.appendChild(li)
+        })
+    })
 
     //add to do
     form.addEventListener('submit', event => {
@@ -29,10 +58,26 @@ function loadEventListeners() {
             li.appendChild(link)
             //shove li into ul 
             taskList.appendChild(li)
+            //store in local storage
+            storeTaskInLocalStorage(taskInput.value)
+
             taskInput.value = '';
             event.preventDefault();
         }
     });
+
+    //local storage function (add tasks to local storage)
+    const storeTaskInLocalStorage = task => {
+        let tasks;
+        if (localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+        tasks.push(task)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
 
     //remove todo event
     taskList.addEventListener('click', event => {
