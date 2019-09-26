6c1de1da33bd67567604c6bff8cc5d835c6e532d
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-25 22:22:28 -0700

    need formula

--- loanCalculator/script.js
+++ loanCalculator/script.js
@@ -1,4 +1,23 @@
-const amount = document.querySelector('#amount');
-amount.value = 7
+const loanForm = document.querySelector('#loan-form');
 
-console.log(amount.value)
+loanForm.addEventListener('submit', calculateResults);
+
+
+function calculateResults(event) {
+    console.log('calculating...');
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthyPayment = document.querySelector('#monthy-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+
+    //gross
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //compute monthly payment
+
+    event.preventDefault();
+}
